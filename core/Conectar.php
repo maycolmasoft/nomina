7f617c6e64ceef6a4bfe3af1f686d8f47989c189
Maycol <Maycol@STEVEN-PC.CAPREMCI.INT>
2019-02-13 09:03:54 -0500

    dfgfdg

--- core/Conectar.php
+++ core/Conectar.php
@@ -18,7 +18,7 @@ class Conectar{
         
         if($this->driver=="pgsql" || $this->driver==null){
        
-        	$con = pg_connect("host=localhost port=5432 dbname=nomina user=postgres password=Clave1*");
+        	$con = pg_connect("host=192.168.1.128 port=5432 dbname=nomina user=postgres password=Capremci2018");
         	if(!$con){
         		echo "No se puedo Conectar a la Base";
         		exit();
@@ -39,7 +39,7 @@ class Conectar{
         	
         	try
         	{
-        		$pdo = new PDO('pgsql:host=localhost;port=5432;dbname=nomina', 'postgres', 'Clave1*' );
+        		$pdo = new PDO('pgsql:host=192.168.1.128;port=5432;dbname=nomina', 'postgres', 'Capremci2018' );
             	
             
          
